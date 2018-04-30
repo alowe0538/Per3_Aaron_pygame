4cdbfe5850301a047a1bdc18a4a129241982ffba
Aaron Lowe <ALowe0538@student.pps.net>
2018-04-30 13:35:45 -0700

    Started working on new function for determining a proper screen color based on the time of day.

--- game_window.py
+++ game_window.py
@@ -1,5 +1,21 @@
 import pygame as pg
 
+def get_brightness(hour):
+    """
+    Sets the brightness based off of the hour of the day in military time.
+    """
+
+    if not hour.isinstance(Integer):
+        raise TypeError("the specified hour is not an int");
+    hour %= 24
+    hour += 1
+
+    SUNRISE = 6
+    SUNSET = 20
+
+    return (0, 0, 255 / hour)
+
+
 pg.init()
 
 screen = pg.display.set_mode((500, 500))
@@ -9,3 +25,8 @@ while running:
     event = pg.event.poll()
     if event.type == pg.QUIT:
         running = False
+
+        # clear the screen
+        screen.fill(get_brightness(6))
+        pg.display.flip()
+
