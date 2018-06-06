9b966f27b81ec0f489b2080416daad87446c3b4a
bcao6124 <bcao6124@apps4pps.net>
2018-06-06 20:27:34 +0000

    Linked up the LED and buttons

--- lightbuttoncontrols.py
+++ lightbuttoncontrols.py
@@ -11,13 +11,25 @@ middle_light=LED(22)
 left_light=LED(27)
  
 def main_thruster_activated():
-    return main_thruster.is_pressed()
+    if main_thruster.is_pressed:
+        middle_light.on()
+    else:
+        middle_light.off()  
+    return main_thruster.is_pressed
  
-def left_thruster_activate():
-    return right_thruster.is_pressed()
+def left_thruster_activated():
+    if left_thruster.is_pressed:
+        left_light.on()
+    else:
+        left_light.off()
+    return left_thruster.is_pressed
  
-def right_thruster_active():
-    return right_thruster.is_pressed()
+def right_thruster_activated():
+    if right_thruster.is_pressed:
+        right_light.on()
+    else:
+        right_light.off()
+    return right_thruster.is_pressed
  
 def crashed():
     lose.on()
